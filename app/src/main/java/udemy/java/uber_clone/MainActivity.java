53af5aaa0285cac7d066bff7c747c89572caf2af
sergi <a18074@alunos.ipca.pt>
2024-08-28 17:16:10 +0100

    Work Done: - Adding three UI layouts to the project: a menu with options, a login and register form.
    * Notes:
    - ...
    
    * Add items:
    - MainActivity.java
    - activity_login.xml
    - activity_maps.xml
    - activity_register.xml
    
    * Updated items:
    - AndroidManifest.xml
    - MainActivity.java
    - strings.xml
    - build.gradle.kts
    - colors.xml
    - libs.versions.toml
    - activity_main.xml
    - themes.xml
    - logo.png
    - fundo.png
    - dw_recangle_green.xml
    - dw_rectangle_grey_green.xml
    
    * Removed:
    - ...

--- app/src/main/java/udemy/java/uber_clone/MainActivity.java
+++ app/src/main/java/udemy/java/uber_clone/MainActivity.java
@@ -1,6 +1,8 @@
 package udemy.java.uber_clone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Objects;
+
+import udemy.java.uber_clone.activity.LoginActivity;
+import udemy.java.uber_clone.activity.RegisterActivity;
+import udemy.java.uber_clone.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private Button buttonSignIn;
+    private Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        buttonRegister = binding.buttonRegister;
+        buttonSignIn = binding.buttonSignIn;
+
+        buttonRegister.setOnClickListener(v -> {
+            // Open Register Activity
+            startActivity(new Intent(this, RegisterActivity.class));
+        });
+
+        buttonSignIn.setOnClickListener(v -> {
+            // Open Sign In Activity
+            startActivity(new Intent(this, LoginActivity.class));
         });
     }
 }
---
+++ app/src/main/java/udemy/java/uber_clone/activity/RegisterActivity.java
@@ -0,0 +1,31 @@
+package udemy.java.uber_clone.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
+
+import udemy.java.uber_clone.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setBackgroundDrawable(ContextCompat.getDrawable(this, R.color.green_100));
+            actionBar.setTitle("Criar uma conta");
+        }
+
+    }
+}
